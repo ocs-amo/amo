6f4f485d9fd18ff9a1d0c87191d5e34f716a6824
yuto ichinose <yuto.0526.sano@gmail.com>
2024-07-26 19:44:03 +0900

    chore: linter追加・フォーマット

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -1,26 +1,26 @@
-import { randomInteger } from "@/utils/random";
-import { Box, Card, CardBody, CardHeader, Divider, Flex, Grid, GridItem, Heading, HStack, Image, InfoIcon, Spacer, Tag, Text, VStack } from "@yamada-ui/react";
+import { randomInteger } from '@/utils/random';
+import { Box, Card, CardBody, CardHeader, Divider, Flex, Grid, GridItem, Heading, HStack, Image, InfoIcon, Tag, Text, VStack } from '@yamada-ui/react';
 
 const notificationMockData = [
-  { id: 1, icon: "https://i.pravatar.cc/100", title: "今月からの新メンバー", createdAt: "2024-06-02 9:00", type: "alert" },
-  { id: 2, icon: "https://picsum.photos/seed/picsum/100/100", title: "やってみたいこと募集", createdAt: "2024-06-02 9:00", type: "" },
-]
+  { id: 1, icon: 'https://i.pravatar.cc/100', title: '今月からの新メンバー', createdAt: '2024-06-02 9:00', type: 'alert' },
+  { id: 2, icon: 'https://picsum.photos/seed/picsum/100/100', title: 'やってみたいこと募集', createdAt: '2024-06-02 9:00', type: '' },
+];
 
 const circleMockData = [
-  { id: 1, thumbnail: "", name: "プログラミングサークル", people: 10, activityDay: "木・金" },
-  { id: 2, thumbnail: "", name: "ゲームサークル", people: 10, activityDay: "木・金" },
-  { id: 3, thumbnail: "", name: "イラストサークル", people: 10, activityDay: "木・金" }
-]
+  { id: 1, thumbnail: '', name: 'プログラミングサークル', people: 10, activityDay: '木・金' },
+  { id: 2, thumbnail: '', name: 'ゲームサークル', people: 10, activityDay: '木・金' },
+  { id: 3, thumbnail: '', name: 'イラストサークル', people: 10, activityDay: '木・金' }
+];
 
 const calendarMockData = [
-  { date: "2", events: [] },
-  { date: "3", events: [{ title: "ゲームサークル" }] },
-  { date: "4", events: [] },
-  { date: "5", events: [] },
-  { date: "6", events: [{ title: "プログラミングサークル" }] },
-  { date: "7", events: [] },
-  { date: "8", events: [] },
-]
+  { date: '2', events: [] },
+  { date: '3', events: [{ title: 'ゲームサークル' }] },
+  { date: '4', events: [] },
+  { date: '5', events: [] },
+  { date: '6', events: [{ title: 'プログラミングサークル' }] },
+  { date: '7', events: [] },
+  { date: '8', events: [] },
+];
 
 export default function Home() {
   return (
@@ -28,7 +28,7 @@ export default function Home() {
       <VStack>
         <Heading as="h2" size="lg">ようそこ！</Heading>
         <Divider w="full" borderWidth="2px" orientation="horizontal" variant="solid" />
-        <Heading as="h2" size="lg">R4SA00　加古 林檎</Heading>
+        <Heading as="h2" size="lg">R4SA00{'　'}加古 林檎</Heading>
       </VStack>
       <Grid w="full" flexGrow={1} templateAreas={{
         base: `
@@ -43,18 +43,18 @@ export default function Home() {
         "calendar"
         `
       }} gap="lg">
-        <GridItem as={Card} area="notification" minW={{ base: "md", sm: "full" }}>
+        <GridItem as={Card} area="notification" minW={{ base: 'md', sm: 'full' }}>
           <CardHeader><Heading as="h3" size="sm">お知らせ</Heading></CardHeader>
           <CardBody>
             <VStack overflowY="auto" gap="md">
               {notificationMockData.map((data) => (
                 <HStack key={data.id} p="sm" borderWidth={1}>
                   <Box>
-                    <Image src={data.icon} w="full" />
+                    <Image src={data.icon} w="full" alt='icon image' />
                   </Box>
                   <VStack>
                     <HStack gap="sm">
-                      {data.type === "alert" ? <InfoIcon size="lg" color="primary" /> : undefined}
+                      {data.type === 'alert' ? <InfoIcon size="lg" color="primary" /> : undefined}
                       <Heading size="xs" as="h4">
                         {data.title}
                       </Heading>
@@ -71,11 +71,11 @@ export default function Home() {
         <GridItem as={Card} area="circles">
           <CardHeader><Heading as="h3" size="sm">所属サークル</Heading></CardHeader>
           <CardBody>
-            <Grid gridTemplateColumns={{ base: "repeat(3, 1fr)", xl: "repeat(2, 1fr)", lg: "repeat(1, 1fr)", md: "repeat(2, 1fr)", sm: "repeat(1, 1fr)" }} gap="md" w="full">
+            <Grid gridTemplateColumns={{ base: 'repeat(3, 1fr)', xl: 'repeat(2, 1fr)', lg: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)', sm: 'repeat(1, 1fr)' }} gap="md" w="full">
               {
                 circleMockData.map(data => (
                   <VStack key={data.id} gap={0} borderWidth={1} w="full" as={GridItem}>
-                    <Image w="full" h="auto" src={`https://picsum.photos/seed/${randomInteger(100)}/200/100`} />
+                    <Image w="full" h="auto" src={`https://picsum.photos/seed/${randomInteger(100)}/200/100`} alt='preview image' />
                     <Box p="sm">
                       <Heading as="h4" size="xs">{data.name}</Heading>
                       <Text>人数：{data.people}</Text>
--- nextjs-app/components/layouts/app-layout.tsx
+++ nextjs-app/components/layouts/app-layout.tsx
@@ -1,105 +1,105 @@
-"use client"
-import { Box, HStack, IconButton, VStack } from "@yamada-ui/react";
-import { BellIcon, CalendarDaysIcon, FileDigitIcon, HomeIcon, MessageCircleMoreIcon, SettingsIcon, UsersIcon } from '@yamada-ui/lucide'
-import Link from "next/link";
-import { FC, ReactNode } from "react";
-import { usePathname } from "next/navigation";
+'use client';
+import { Box, HStack, IconButton, VStack } from '@yamada-ui/react';
+import { BellIcon, CalendarDaysIcon, FileDigitIcon, HomeIcon, MessageCircleMoreIcon, SettingsIcon, UsersIcon } from '@yamada-ui/lucide';
+import Link from 'next/link';
+import type { FC, ReactNode } from 'react';
+import { usePathname } from 'next/navigation';
 
 export const AppLayout: FC<{ children?: ReactNode }> = ({ children }) => {
 
-    const pathname = usePathname()
+  const pathname = usePathname();
 
-    return pathname !== '/login' ? <HStack w='100vw' h='100dvh' gap={0}>
-        <VStack
-            w="fit-content"
-            h="full"
-            p="sm"
-            borderRight="1px solid"
-            borderRightColor={["blackAlpha.500", "whiteAlpha.500"]}
-            justifyContent='space-between'
-        >
-            <VStack>
-                <IconButton
-                    w="50px"
-                    h="50px"
-                    justifyContent='center'
-                    alignItems="center"
-                    as={Link}
-                    variant='ghost'
-                    href='/'
-                    icon={<HomeIcon size="2xl" />}
-                    title="ホーム"
-                />
-                <IconButton
-                    w="50px"
-                    h="50px"
-                    justifyContent='center'
-                    alignItems="center"
-                    as={Link}
-                    variant='ghost'
-                    href='/'
-                    icon={<UsersIcon size="2xl" />}
-                    title="サークル一覧"
-                />
-                <IconButton
-                    w="50px"
-                    h="50px"
-                    justifyContent='center'
-                    alignItems="center"
-                    as={Link}
-                    variant='ghost'
-                    href='/'
-                    icon={<MessageCircleMoreIcon size="2xl" />}
-                    title="DM"
-                />
-                <IconButton
-                    w="50px"
-                    h="50px"
-                    justifyContent='center'
-                    alignItems="center"
-                    as={Link}
-                    variant='ghost'
-                    href='/'
-                    icon={<CalendarDaysIcon size="2xl" />}
-                    title="カレンダー"
-                />
-                <IconButton
-                    w="50px"
-                    h="50px"
-                    justifyContent='center'
-                    alignItems="center"
-                    as={Link}
-                    variant='ghost'
-                    href=''
-                    icon={<FileDigitIcon size="2xl" />}
-                    title="今日のイベント"
-                />
-                <IconButton
-                    w="50px"
-                    h="50px"
-                    justifyContent='center'
-                    alignItems="center"
-                    as={Link}
-                    variant='ghost'
-                    href=''
-                    icon={<BellIcon size="2xl" />}
-                    title="通知"
-                />
-            </VStack>
-            <IconButton
-                w="50px"
-                h="50px"
-                justifyContent='center'
-                alignItems="center"
-                as={Link}
-                variant='ghost'
-                href='/'
-                icon={<SettingsIcon size="2xl" />}
-                title="設定"
-            />
-        </VStack>
-        <Box w='full' h='full' overflowY='auto'>
-            {children}
-        </Box>
-    </HStack> : children
-}
+  return pathname !== '/login' ? <HStack w='100vw' h='100dvh' gap={0}>
+    <VStack
+      w="fit-content"
+      h="full"
+      p="sm"
+      borderRight="1px solid"
+      borderRightColor={['blackAlpha.500', 'whiteAlpha.500']}
+      justifyContent='space-between'
+    >
+      <VStack>
+        <IconButton
+          w="50px"
+          h="50px"
+          justifyContent='center'
+          alignItems="center"
+          as={Link}
+          variant='ghost'
+          href='/'
+          icon={<HomeIcon size="2xl" />}
+          title="ホーム"
+        />
+        <IconButton
+          w="50px"
+          h="50px"
+          justifyContent='center'
+          alignItems="center"
+          as={Link}
+          variant='ghost'
+          href='/'
+          icon={<UsersIcon size="2xl" />}
+          title="サークル一覧"
+        />
+        <IconButton
+          w="50px"
+          h="50px"
+          justifyContent='center'
+          alignItems="center"
+          as={Link}
+          variant='ghost'
+          href='/'
+          icon={<MessageCircleMoreIcon size="2xl" />}
+          title="DM"
+        />
+        <IconButton
+          w="50px"
+          h="50px"
+          justifyContent='center'
+          alignItems="center"
+          as={Link}
+          variant='ghost'
+          href='/'
+          icon={<CalendarDaysIcon size="2xl" />}
+          title="カレンダー"
+        />
+        <IconButton
+          w="50px"
+          h="50px"
+          justifyContent='center'
+          alignItems="center"
+          as={Link}
+          variant='ghost'
+          href=''
+          icon={<FileDigitIcon size="2xl" />}
+          title="今日のイベント"
+        />
+        <IconButton
+          w="50px"
+          h="50px"
+          justifyContent='center'
+          alignItems="center"
+          as={Link}
+          variant='ghost'
+          href=''
+          icon={<BellIcon size="2xl" />}
+          title="通知"
+        />
+      </VStack>
+      <IconButton
+        w="50px"
+        h="50px"
+        justifyContent='center'
+        alignItems="center"
+        as={Link}
+        variant='ghost'
+        href='/'
+        icon={<SettingsIcon size="2xl" />}
+        title="設定"
+      />
+    </VStack>
+    <Box w='full' h='full' overflowY='auto'>
+      {children}
+    </Box>
+  </HStack> : children;
+};
