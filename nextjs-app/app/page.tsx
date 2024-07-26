2ecec727a5505dbb913ff973c35d4dc29f0dd96d
yuto ichinose <yuto.0526.sano@gmail.com>
2024-07-26 13:02:31 +0900

    feat: カレンダー実装・どうしてもはみ出る

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -1,20 +1,30 @@
 import { randomInteger } from "@/utils/random";
-import { Box, Card, CardBody, CardHeader, Divider, Flex, Grid, GridItem, Heading, HStack, Image, InfoIcon, Text, VStack } from "@yamada-ui/react";
+import { Box, Card, CardBody, CardHeader, Divider, Flex, Grid, GridItem, Heading, HStack, Image, InfoIcon, Spacer, Tag, Text, VStack } from "@yamada-ui/react";
 
 const notificationMockData = [
-  { id: 1, icon: "https://i.pravatar.cc/100", title: "今月からの新メンバー", createdAt: "2024-06-02 9:00", type: "alert"},
-  { id: 2, icon: "https://picsum.photos/seed/picsum/100/100", title: "やってみたいこと募集", createdAt: "2024-06-02 9:00", type: ""},
+  { id: 1, icon: "https://i.pravatar.cc/100", title: "今月からの新メンバー", createdAt: "2024-06-02 9:00", type: "alert" },
+  { id: 2, icon: "https://picsum.photos/seed/picsum/100/100", title: "やってみたいこと募集", createdAt: "2024-06-02 9:00", type: "" },
 ]
 
 const circleMockData = [
-  {id: 1, thumbnail: "", name: "プログラミングサークル", people: 10, activityDay: "木・金" },
-  {id: 2, thumbnail: "", name: "ゲームサークル", people: 10, activityDay: "木・金" },
-  {id: 3, thumbnail: "", name: "イラストサークル", people: 10, activityDay: "木・金" }
+  { id: 1, thumbnail: "", name: "プログラミングサークル", people: 10, activityDay: "木・金" },
+  { id: 2, thumbnail: "", name: "ゲームサークル", people: 10, activityDay: "木・金" },
+  { id: 3, thumbnail: "", name: "イラストサークル", people: 10, activityDay: "木・金" }
+]
+
+const calendarMockData = [
+  { date: "2", events: [] },
+  { date: "3", events: [{ title: "ゲームサークル" }] },
+  { date: "4", events: [] },
+  { date: "5", events: [] },
+  { date: "6", events: [{ title: "プログラミングサークル" }] },
+  { date: "7", events: [] },
+  { date: "8", events: [] },
 ]
 
 export default function Home() {
   return (
-    <VStack w="full" h="full" p="md">
+    <VStack w="full" h="fit-content" p="md">
       <VStack>
         <Heading as="h2" size="lg">ようそこ！</Heading>
         <Divider w="full" borderWidth="2px" orientation="horizontal" variant="solid" />
@@ -33,7 +43,7 @@ export default function Home() {
         "calendar"
         `
       }} gap="lg">
-        <GridItem as={Card} area="notification" minW={{base:"md", sm: "full"}}>
+        <GridItem as={Card} area="notification" minW={{ base: "md", sm: "full" }}>
           <CardHeader><Heading as="h3" size="sm">お知らせ</Heading></CardHeader>
           <CardBody>
             <VStack overflowY="auto" gap="md">
@@ -61,11 +71,11 @@ export default function Home() {
         <GridItem as={Card} area="circles">
           <CardHeader><Heading as="h3" size="sm">所属サークル</Heading></CardHeader>
           <CardBody>
-            <Grid gridTemplateColumns={{base: "repeat(3, 1fr)", xl: "repeat(2, 1fr)", lg: "repeat(1, 1fr)", md: "repeat(2, 1fr)", sm: "repeat(1, 1fr)"}} gap="md"  w="full">
+            <Grid gridTemplateColumns={{ base: "repeat(3, 1fr)", xl: "repeat(2, 1fr)", lg: "repeat(1, 1fr)", md: "repeat(2, 1fr)", sm: "repeat(1, 1fr)" }} gap="md" w="full">
               {
                 circleMockData.map(data => (
-                  <VStack key={data.id} gap={0} borderWidth={1} w="full">
-                    <Image w="full" h="auto"  src={`https://picsum.photos/seed/${randomInteger(100)}/200/100`} />
+                  <VStack key={data.id} gap={0} borderWidth={1} w="full" as={GridItem}>
+                    <Image w="full" h="auto" src={`https://picsum.photos/seed/${randomInteger(100)}/200/100`} />
                     <Box p="sm">
                       <Heading as="h4" size="xs">{data.name}</Heading>
                       <Text>人数：{data.people}</Text>
@@ -79,7 +89,26 @@ export default function Home() {
         </GridItem>
         <GridItem as={Card} area="calendar">
           <CardHeader><Heading as="h3" size="sm">カレンダー</Heading></CardHeader>
-          <CardBody></CardBody>
+          <CardBody>
+            <HStack w="full" gap={0} flexDir={{sm: "column"}}>
+              {
+                calendarMockData.map((data, index) => (
+                  <VStack h="full" flex={1} borderWidth={1} borderLeftWidth={ index === 0 ? undefined : 0 } p="md">
+                    <Box>
+                      {data.date}
+                    </Box>
+                    <VStack h="sm">
+                      {
+                        data.events.map((event, i) => (
+                          <Tag key={i}>{event.title}</Tag>
+                        ))
+                      }
+                    </VStack>
+                  </VStack>
+                ))
+              }
+            </HStack>
+          </CardBody>
         </GridItem>
       </Grid>
     </VStack>
