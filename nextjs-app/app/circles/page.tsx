0b548359f70b1c56e77464c919b691950db0ca8b
yuto ichinose <yuto.0526.sano@gmail.com>
2024-08-01 14:20:20 +0900

    fix: スクロールボタン追加・作成ボタン移動

--- nextjs-app/app/circles/page.tsx
+++ nextjs-app/app/circles/page.tsx
@@ -1,6 +1,8 @@
+'use client';
 import { CircleCard } from '@/components/data-display/circle-card';
-import { SearchIcon } from '@yamada-ui/lucide';
-import { Button, Grid, Heading, HStack, Input, InputGroup, InputLeftElement, VStack } from '@yamada-ui/react';
+import { SearchIcon, TriangleIcon } from '@yamada-ui/lucide';
+import { Box, Button, Grid, Heading, HStack, IconButton, Input, InputGroup, InputLeftElement, VStack } from '@yamada-ui/react';
+import { useRef } from 'react';
 
 const circleMockData = [
   { id: 1, thumbnail: '', name: 'プログラミングサークル', people: 10, activityDay: '木・金' },
@@ -10,28 +12,47 @@ const circleMockData = [
   { id: 5, thumbnail: '', name: 'スポーツサークル', people: 10, activityDay: '木・金' },
   { id: 6, thumbnail: '', name: 'サイクリングサークル', people: 10, activityDay: '木・金' },
   { id: 7, thumbnail: '', name: '旅行サークル', people: 10, activityDay: '木・金' },
+  { id: 8, thumbnail: '', name: '旅行サークル', people: 10, activityDay: '木・金' },
+  { id: 9, thumbnail: '', name: '旅行サークル', people: 10, activityDay: '木・金' },
+  { id: 10, thumbnail: '', name: '旅行サークル', people: 10, activityDay: '木・金' },
+  { id: 11, thumbnail: '', name: '旅行サークル', people: 10, activityDay: '木・金' },
+  { id: 12, thumbnail: '', name: '旅行サークル', people: 10, activityDay: '木・金' },
+  { id: 13, thumbnail: '', name: '旅行サークル', people: 10, activityDay: '木・金' },
 ];
 
 const CirclesPage = () => {
+
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+
+  const handleScroll = () => {
+    scrollRef.current?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  };
+
   return (
     <>
-      <VStack w="full" h="full" p="md">
-        <HStack as="header" alignItems={{ base: 'center', sm: 'start' }} flexDir={{ sm: 'column' }}>
-          <Heading flex={1} as="h1" size="lg" >サークル一覧</Heading>
-          <InputGroup flex={1}>
-            <InputLeftElement>
-              <SearchIcon color="gray.500" />
-            </InputLeftElement>
-            <Input type="search" placeholder="サークルを検索" pl="lg" />
-          </InputGroup>
-        </HStack>
-        <Grid gridTemplateColumns={{ base: 'repeat(4, 1fr)', lg: 'repeat(3, 1fr)', md: 'repeat(2, 1fr)', sm: 'repeat(1, 1fr)' }} gap="md" w="full">
+      <VStack ref={scrollRef} w="full" h="fit-content" px="md" gap={0}>
+        <VStack position="sticky" py="md" top={0} backgroundColor="Menu" as="header">
+          <HStack alignItems={{ base: 'center', sm: 'start' }} flexDir={{ sm: 'column' }}>
+            <Heading flex={1} as="h1" size="lg" >サークル一覧</Heading>
+            <InputGroup flex={1}>
+              <InputLeftElement>
+                <SearchIcon color="gray.500" />
+              </InputLeftElement>
+              <Input type="search" placeholder="サークルを検索" pl="lg" />
+            </InputGroup>
+          </HStack>
+          <Box textAlign="right"><Button>サークル作成</Button></Box>
+        </VStack>
+        <Grid pb="md" gridTemplateColumns={{ base: 'repeat(4, 1fr)', lg: 'repeat(3, 1fr)', md: 'repeat(2, 1fr)', sm: 'repeat(1, 1fr)' }} gap="md" w="full">
           {
             circleMockData.map(data => <CircleCard key={data.id} data={data} />)
           }
         </Grid>
       </VStack>
-      <Button position="fixed" bottom="8" right="8">サークル作成</Button>
+      <IconButton position="fixed" colorScheme='primary' bottom="8" right="8" icon={<TriangleIcon />} onClick={handleScroll} />
     </>
   );
 };
