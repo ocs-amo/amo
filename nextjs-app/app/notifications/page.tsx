09155082a5c3e6066d112eeeca1c0eedca71f004
yuto ichinose <yuto.0526.sano@gmail.com>
2024-08-01 19:31:07 +0900

    fix: gap修正

--- nextjs-app/app/notifications/page.tsx
+++ nextjs-app/app/notifications/page.tsx
@@ -1,7 +1,7 @@
 import { Box, Divider, Flex, Heading, HStack, Image, InfoIcon, Text, VStack } from '@yamada-ui/react';
 
 const notificationMockData = [
-  { id: 1, icon: 'https://i.pravatar.cc/100', title: '天文サークルが新たに作成されました！', createdAt: '1分前', type: 'alert', description: ''  },
+  { id: 1, icon: 'https://i.pravatar.cc/100', title: '天文サークルが新たに作成されました！', createdAt: '1分前', type: 'alert', description: '' },
   { id: 2, icon: 'https://picsum.photos/seed/picsum/100/100', title: 'プログラミングサークル', createdAt: '4時間前', type: '', description: '6/7 16:00～' },
   { id: 3, icon: 'https://picsum.photos/seed/picsum/100/100', title: '料理サークル', createdAt: '2日前', type: '', description: '7/3 16:00 ロビー集合' },
 ];
@@ -13,7 +13,7 @@ const NotificationsPage = () => {
       <Divider w="full" borderWidth="2px" orientation="horizontal" variant="solid" />
     </VStack>
     {notificationMockData.map((data) => (
-      <HStack key={data.id} w="full" flexDir={{sm: "column"}} gap={{sm: "sm"}}>
+      <HStack key={data.id} w="full" flexDir={{ sm: 'column' }} gap={{ sm: 'sm', base: 'md' }}>
         <HStack flexGrow={1} p="sm" borderWidth={1} w="full">
           <Box>
             <Image src={data.icon} w="full" alt='icon image' />
@@ -30,7 +30,7 @@ const NotificationsPage = () => {
             </Text>
           </VStack>
         </HStack>
-        <Flex w={{base: "20", sm: "full"}} justifyContent={{base: "left", sm: "right"}}>
+        <Flex w={{ base: '20', sm: 'full' }} justifyContent={{ base: 'left', sm: 'right' }}>
           {data.createdAt}
         </Flex>
       </HStack>
