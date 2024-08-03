5850d2e14558ac3f6e7b0f5e00f72c975d44302a
yuto ichinose <yuto.0526.sano@gmail.com>
2024-08-03 17:49:23 +0900

    feat: サークル作成のフォームのみ

---
+++ nextjs-app/app/circles/create/page.tsx
@@ -0,0 +1,59 @@
+'use client';
+import { CameraIcon } from '@yamada-ui/lucide';
+import type { SelectItem} from '@yamada-ui/react';
+import { Center, FormControl, Heading, Input, Label, Select, Text, VStack } from '@yamada-ui/react';
+
+const items: SelectItem[] = [
+  {
+    label: '乃木',
+    value: '乃木'
+  },
+  {
+    label: '西村',
+    value: '西村'
+  },
+  {
+    label: '木村',
+    value: '木村'
+  },
+];
+
+const CircleCreate = () => {
+  return <VStack gap={0}>
+    <Center w="full" h="40" backgroundColor="gray.100">
+      <VStack alignItems="center" gap={0}>
+        <CameraIcon size="9xl" color="gray" />
+        <Text color="gray" size="xl">ヘッダー画像</Text>
+      </VStack>
+    </Center>
+    <VStack p="md">
+      <Heading as="h1" size="lg">サークル作成</Heading>
+      <form onSubmit={e => e.preventDefault()}>
+        <VStack px="md">
+          <FormControl isRequired display="flex" flexDirection={{base: 'row', md: 'column'}} gap={{base: '2xl', md: 'md'}} maxW="2xl">
+            <Label flexGrow={1}>サークル名</Label>
+            <Input type="text" w={{base: 'md', md: 'full'}} placeholder="サークル名を入力" />
+          </FormControl>
+          <FormControl display="flex" flexDirection={{base: 'row', md: 'column'}} gap={{base: '2xl', md: 'md'}} maxW="2xl">
+            <Label flexGrow={1}>講師</Label>
+            <Select w={{base: 'md', md: 'full'}} placeholder="講師を選択" items={items} />
+          </FormControl>
+          <FormControl display="flex" flexDirection={{base: 'row', md: 'column'}} gap={{base: '2xl', md: 'md'}} maxW="2xl">
+            <Label flexGrow={1}>タグ</Label>
+            <Input type="text" w={{base: 'md', md: 'full'}} placeholder="タグを入力" />
+          </FormControl>
+          <FormControl display="flex" flexDirection={{base: 'row', md: 'column'}} gap={{base: '2xl', md: 'md'}} maxW="2xl">
+            <Label flexGrow={1}>活動場所</Label>
+            <Input type="text" w={{base: 'md', md: 'full'}} placeholder="活動場所を入力" />
+          </FormControl>
+          <FormControl display="flex" flexDirection={{base: 'row', md: 'column'}} gap={{base: '2xl', md: 'md'}} maxW="2xl">
+            <Label flexGrow={1}>活動時間</Label>
+            <Input type="text" w={{base: 'md', md: 'full'}} placeholder="活動時間を入力" />
+          </FormControl>
+        </VStack>
+      </form>
+    </VStack>
+  </VStack>;
+};
+
+export default CircleCreate;
