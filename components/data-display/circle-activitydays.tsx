01153085992a42c117943585bde4025f02493368
nakata0047 <n.craft0047r@gmail.com>
2024-10-26 01:31:59 +0900

    feat:活動日程画面(仮データ配置)

--- components/data-display/circle-activitydays.tsx
+++ components/data-display/circle-activitydays.tsx
@@ -1,9 +1,17 @@
 "use client"
 import { MonthPicker } from "@yamada-ui/calendar"
-import { PlusIcon } from "@yamada-ui/lucide"
+import { Car, PlusIcon } from "@yamada-ui/lucide"
+import { HiOutlineDotsCircleHorizontal } from "react-icons/hi";
 import type { FC } from "@yamada-ui/react"
-import { HStack, IconButton, VStack } from "@yamada-ui/react"
+import { Card, CardBody, Flex, GridItem, HStack, IconButton, Text, VStack } from "@yamada-ui/react"
 import "dayjs/locale/ja"
+
+const activitys = [
+                    ["7","活動日","402","16:00","18:00",],
+                    ["8","ミーティング","402","16:00","18:00",],
+                    ["12","活動日","402","16:00","18:00",],
+                    ["14","ミーティング","402","16:00","18:00",],
+                  ]; //活動日、タイトル、教室番号、開始時間、終了時間
 
 export const CircleActivitydays: FC = () => {
   return (
@@ -12,7 +20,25 @@ export const CircleActivitydays: FC = () => {
         <MonthPicker w="md" locale="ja" />
         <IconButton icon={<PlusIcon />} />
       </HStack>
-      <VStack></VStack>
+      <VStack>
+        {activitys.map(row =>
+        <GridItem >
+          <Card variant="outline">
+            <CardBody>
+                     
+              <HStack>
+                <Card variant="outline" padding="10px">{row[0]}</Card>
+                <Text>{row[1]}</Text>
+                <Text>{row[2]}教室</Text>
+                <Text>{row[3]}～{row[4]}</Text>
+                <IconButton icon={<HiOutlineDotsCircleHorizontal />} />
+              </HStack>
+            
+            </CardBody>
+          </Card>
+        </GridItem>
+        )}
+      </VStack>
     </VStack>
   )
 }
