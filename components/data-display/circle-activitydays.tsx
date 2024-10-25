11cfadbddf68d8c3a983f3c9b4ecd817fcfcb25e
nakata0047 <n.craft0047r@gmail.com>
2024-10-26 02:03:02 +0900

    fix:レイアウト調整

--- components/data-display/circle-activitydays.tsx
+++ components/data-display/circle-activitydays.tsx
@@ -1,17 +1,25 @@
 "use client"
 import { MonthPicker } from "@yamada-ui/calendar"
-import { Car, PlusIcon } from "@yamada-ui/lucide"
-import { HiOutlineDotsCircleHorizontal } from "react-icons/hi";
+import { PlusIcon } from "@yamada-ui/lucide"
 import type { FC } from "@yamada-ui/react"
-import { Card, CardBody, Flex, GridItem, HStack, IconButton, Text, VStack } from "@yamada-ui/react"
+import {
+  Card,
+  CardBody,
+  GridItem,
+  HStack,
+  IconButton,
+  Text,
+  VStack,
+} from "@yamada-ui/react"
+import { HiOutlineDotsCircleHorizontal } from "react-icons/hi"
 import "dayjs/locale/ja"
 
 const activitys = [
-                    ["7","活動日","402","16:00","18:00",],
-                    ["8","ミーティング","402","16:00","18:00",],
-                    ["12","活動日","402","16:00","18:00",],
-                    ["14","ミーティング","402","16:00","18:00",],
-                  ]; //活動日、タイトル、教室番号、開始時間、終了時間
+  ["7", "活動日", "402", "16:00", "18:00"],
+  ["8", "ミーティング", "402", "16:00", "18:00"],
+  ["12", "活動日", "402", "16:00", "18:00"],
+  ["14", "ミーティング", "402", "16:00", "18:00"],
+] //活動日、タイトル、教室番号、開始時間、終了時間
 
 export const CircleActivitydays: FC = () => {
   return (
@@ -21,23 +29,30 @@ export const CircleActivitydays: FC = () => {
         <IconButton icon={<PlusIcon />} />
       </HStack>
       <VStack>
-        {activitys.map(row =>
-        <GridItem >
-          <Card variant="outline">
-            <CardBody>
-                     
-              <HStack>
-                <Card variant="outline" padding="10px">{row[0]}</Card>
-                <Text>{row[1]}</Text>
-                <Text>{row[2]}教室</Text>
-                <Text>{row[3]}～{row[4]}</Text>
-                <IconButton icon={<HiOutlineDotsCircleHorizontal />} />
-              </HStack>
-            
-            </CardBody>
-          </Card>
-        </GridItem>
-        )}
+        {activitys.map((row) => (
+          <GridItem>
+            <Card variant="outline">
+              <CardBody>
+                <HStack justifyContent="space-between" w="full">
+                  <HStack>
+                    <Card variant="outline" padding="10px">
+                      {row[0]}
+                    </Card>
+                    <Text>{row[1]}</Text>
+                  </HStack>
+
+                  <HStack>
+                    <Text>{row[2]}教室</Text>
+                    <Text>
+                      {row[3]}～{row[4]}
+                    </Text>
+                    <IconButton icon={<HiOutlineDotsCircleHorizontal />} />
+                  </HStack>
+                </HStack>
+              </CardBody>
+            </Card>
+          </GridItem>
+        ))}
       </VStack>
     </VStack>
   )
