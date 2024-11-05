acbfffbf0ac648b2fb42175d3d4fbfff6b331978
ymmyo5 <epc1126@yahoo.co.jp>
2024-11-05 18:19:49 +0900

    feat: 退会確認ダイアログ追加

--- components/data-display/member-card.tsx
+++ components/data-display/member-card.tsx
@@ -50,13 +50,14 @@ export const MemberCard: FC<MemberCard> = ({
   handleRoleChange,
   handleRemoveMember,
 }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const roleDialogDisclosure = useDisclosure();
+  const removeDialogDisclosure = useDisclosure();
   const [selectedRole, setSelectedRole] = useState<number | null>(null)
   const [selectedRoleName, setSelectedRoleName] = useState<string | null>(null)
 
   // 権限変更モーダルを開く
-  const openConfirmDialog = (newRoleId: number, newRoleName: string) => {
-    onOpen()
+  const openRoleDialog = (newRoleId: number, newRoleName: string) => {
+    roleDialogDisclosure.onOpen()
     setSelectedRole(newRoleId)
     setSelectedRoleName(newRoleName)
   }
@@ -64,12 +65,17 @@ export const MemberCard: FC<MemberCard> = ({
   // 権限変更を確定
   const confirmRoleChange = async () => {
     if (userId && selectedRole !== null) {
-      onClose()
+      roleDialogDisclosure.onClose()
       await handleRoleChange(member.id, selectedRole)
       setSelectedRole(null)
       setSelectedRoleName("")
     }
   }
+
+  // 退会確認モーダルを開く
+  const openRemoveDialog = () => {
+    removeDialogDisclosure.onOpen();
+  };
 
   return (
     <GridItem w="full" rounded="md" as={Card}>
@@ -99,19 +105,19 @@ export const MemberCard: FC<MemberCard> = ({
                 {userRole?.id === 0 && (
                   <>
                     <MenuItem
-                      onClick={() => openConfirmDialog(0, "代表")}
+                      onClick={() => openRoleDialog(0, "代表")}
                       isDisabled={member.role?.id === 0}
                     >
                       代表
                     </MenuItem>
                     <MenuItem
-                      onClick={() => openConfirmDialog(1, "副代表")}
+                      onClick={() => openRoleDialog(1, "副代表")}
                       isDisabled={member.role?.id === 1}
                     >
                       副代表
                     </MenuItem>
                     <MenuItem
-                      onClick={() => openConfirmDialog(2, "一般")}
+                      onClick={() => openRoleDialog(2, "一般")}
                       isDisabled={member.role?.id === 2}
                     >
                       一般
@@ -119,7 +125,7 @@ export const MemberCard: FC<MemberCard> = ({
                     <MenuDivider />
                     <MenuItem
                       color="red"
-                      onClick={() => handleRemoveMember(member.id)}
+                      onClick={() => openRemoveDialog()}
                     >
                       退会
                     </MenuItem>
@@ -130,13 +136,13 @@ export const MemberCard: FC<MemberCard> = ({
                 {userRole?.id === 1 && (
                   <>
                     <MenuItem
-                      onClick={() => openConfirmDialog(1, "副代表")}
+                      onClick={() => openRoleDialog(1, "副代表")}
                       isDisabled={member.role?.id === 1}
                     >
                       副代表
                     </MenuItem>
                     <MenuItem
-                      onClick={() => openConfirmDialog(2, "一般")}
+                      onClick={() => openRoleDialog(2, "一般")}
                       isDisabled={member.role?.id === 2}
                     >
                       一般
@@ -144,7 +150,7 @@ export const MemberCard: FC<MemberCard> = ({
                     <MenuDivider />
                     <MenuItem
                       color="red"
-                      onClick={() => handleRemoveMember(member.id)}
+                      onClick={() => openRemoveDialog()}
                     >
                       退会
                     </MenuItem>
@@ -155,17 +161,32 @@ export const MemberCard: FC<MemberCard> = ({
           ) : undefined}
 
           {/* 権限変更確認モーダル */}
-          <Modal isOpen={isOpen} onClose={onClose}>
+          <Modal isOpen={roleDialogDisclosure.isOpen} onClose={roleDialogDisclosure.onClose}>
             <ModalOverlay />
             <ModalHeader>権限変更の確認</ModalHeader>
             <ModalBody>
-              {member.name} さんの役割を「{member.role.roleName}」から「
+              {member.name} さんの権限を「{member.role.roleName}」から「
               {selectedRoleName}」に変更しますか？
             </ModalBody>
             <ModalFooter>
-              <Button onClick={onClose}>キャンセル</Button>
+              <Button onClick={roleDialogDisclosure.onClose}>キャンセル</Button>
               <Button colorScheme="blue" onClick={confirmRoleChange}>
-                確認
+                変更
+              </Button>
+            </ModalFooter>
+          </Modal>
+
+          {/* 退会確認モーダル */}
+          <Modal isOpen={removeDialogDisclosure.isOpen} onClose={removeDialogDisclosure.onClose}>
+            <ModalOverlay />
+            <ModalHeader>退会の確認</ModalHeader>
+            <ModalBody>
+              {member.name} さんを本当に退会させますか？
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={removeDialogDisclosure.onClose}>キャンセル</Button>
+              <Button colorScheme="blue" onClick={() => handleRemoveMember(member.id)}>
+                退会
               </Button>
             </ModalFooter>
           </Modal>
