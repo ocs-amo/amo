9b29105c0e5f78907e59cba8bb4998d12bf33319
yuto ichinose <yuto.0526.sano@gmail.com>
2024-11-06 12:33:33 +0900

    feat: コンポーネント分割・非メンバー時は非表示

--- app/circles/[circle_id]/activities/new/page.tsx
+++ app/circles/[circle_id]/activities/new/page.tsx
@@ -1,222 +1,29 @@
-"use client"
-import { DatePicker } from "@yamada-ui/calendar"
-import {
-  Button,
-  Center,
-  FormControl,
-  Heading,
-  Input,
-  Label,
-  Textarea,
-  VStack,
-} from "@yamada-ui/react"
-import "dayjs/locale/ja"
-import Link from "next/link"
+import { Center } from "@yamada-ui/react"
+import { auth } from "@/auth"
+import { ActivityForm } from "@/components/forms/activity-form"
+import { getCircleById } from "@/data/circle"
 
 interface Props {
   params: { circle_id?: string }
 }
 
-const Page = ({ params }: Props) => {
+const Page = async ({ params }: Props) => {
   const { circle_id: circleId } = params
-  return (
-    <VStack
-      w="full"
-      h="full"
-      as="form"
-      onSubmit={(e) => e.preventDefault()}
-      p="md"
-    >
-      <VStack p="md" maxW="5xl" m="auto" gap="lg" flexGrow={1}>
-        <Heading>{params.circle_id} 活動日程</Heading>
-        <FormControl
-          display="flex"
-          flexDirection={{ base: "row", md: "column" }}
-          gap={{ base: "2xl", md: "md" }}
-          maxW="2xl"
-          // isInvalid={!!errors.name}
-        >
-          <Label flexGrow={1} isRequired>
-            見出し
-          </Label>
-          <VStack w="auto">
-            <Input
-              type="text"
-              w={{ base: "md", md: "full" }}
-              placeholder="見出しを入力"
-              // {...register("name")}
-            />
-            {/* {errors.name ? (
-                        <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
-                    ) : (
-                        <>
-                            <Spacer />
-                            <Spacer />
-                        </>
-                    )} */}
-          </VStack>
-        </FormControl>
-        <FormControl
-          display="flex"
-          flexDirection={{ base: "row", md: "column" }}
-          gap={{ base: "2xl", md: "md" }}
-          maxW="2xl"
-          // isInvalid={!!errors.name}
-        >
-          <Label flexGrow={1}>内容</Label>
-          <VStack w="auto">
-            <Textarea
-              w={{ base: "md", md: "full" }}
-              placeholder="活動内容を入力"
-              autosize
-              // {...register("name")}
-            />
-            {/* {errors.name ? (
-                        <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
-                    ) : (
-                        <>
-                            <Spacer />
-                            <Spacer />
-                        </>
-                    )} */}
-          </VStack>
-        </FormControl>
-        <FormControl
-          display="flex"
-          flexDirection={{ base: "row", md: "column" }}
-          gap={{ base: "2xl", md: "md" }}
-          maxW="2xl"
-          // isInvalid={!!errors.name}
-        >
-          <Label flexGrow={1} isRequired>
-            日付
-          </Label>
-          <VStack w="auto">
-            <DatePicker
-              locale="ja"
-              w={{ base: "md", md: "full" }}
-              placeholder="YYYY/MM/DD"
-            />
-            {/* {errors.name ? (
-                        <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
-                    ) : (
-                        <>
-                            <Spacer />
-                            <Spacer />
-                        </>
-                    )} */}
-          </VStack>
-        </FormControl>
-        <FormControl
-          display="flex"
-          flexDirection={{ base: "row", md: "column" }}
-          gap={{ base: "2xl", md: "md" }}
-          maxW="2xl"
-          // isInvalid={!!errors.name}
-        >
-          <Label flexGrow={1}>場所</Label>
-          <VStack w="auto">
-            <Input
-              type="text"
-              w={{ base: "md", md: "full" }}
-              placeholder="場所を入力"
-              // {...register("name")}
-            />
-            {/* {errors.name ? (
-                        <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
-                    ) : (
-                        <>
-                            <Spacer />
-                            <Spacer />
-                        </>
-                    )} */}
-          </VStack>
-        </FormControl>
-        <FormControl
-          display="flex"
-          flexDirection={{ base: "row", md: "column" }}
-          gap={{ base: "2xl", md: "md" }}
-          maxW="2xl"
-          // isInvalid={!!errors.name}
-        >
-          <Label flexGrow={1} isRequired>
-            開始時間
-          </Label>
-          <VStack w="auto">
-            <Input
-              type="time"
-              w={{ base: "md", md: "full" }}
-              placeholder="開始時間を入力"
-              // {...register("name")}
-            />
-            {/* {errors.name ? (
-                        <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
-                    ) : (
-                        <>
-                            <Spacer />
-                            <Spacer />
-                        </>
-                    )} */}
-          </VStack>
-        </FormControl>
-        <FormControl
-          display="flex"
-          flexDirection={{ base: "row", md: "column" }}
-          gap={{ base: "2xl", md: "md" }}
-          maxW="2xl"
-          // isInvalid={!!errors.name}
-        >
-          <Label flexGrow={1}>終了時間</Label>
-          <VStack w="auto">
-            <Input
-              type="time"
-              w={{ base: "md", md: "full" }}
-              placeholder="終了時間を入力"
-              // {...register("name")}
-            />
-            {/* {errors.name ? (
-                        <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
-                    ) : (
-                        <>
-                            <Spacer />
-                            <Spacer />
-                        </>
-                    )} */}
-          </VStack>
-        </FormControl>
-        <FormControl
-          display="flex"
-          flexDirection={{ base: "row", md: "column" }}
-          gap={{ base: "2xl", md: "md" }}
-          maxW="2xl"
-          // isInvalid={!!errors.name}
-        >
-          <Label flexGrow={1}>備考</Label>
-          <VStack w="auto">
-            <Textarea
-              w={{ base: "md", md: "full" }}
-              placeholder="備考欄"
-              autosize
-              // {...register("name")}
-            />
-            {/* {errors.name ? (
-                        <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
-                    ) : (
-                        <>
-                            <Spacer />
-                            <Spacer />
-                        </>
-                    )} */}
-          </VStack>
-        </FormControl>
-        <Center gap="md" justifyContent="end">
-          <Button as={Link} href={`/circles/${circleId || ""}`}>
-            キャンセル
-          </Button>
-          <Button type="submit">追加</Button>
-        </Center>
-      </VStack>
-    </VStack>
+  const session = await auth()
+  const circle = await getCircleById(circleId || "")
+  const isMember = circle?.members?.some(
+    (member) => member.id === session?.user?.id,
+  )
+  return isMember ? (
+    <ActivityForm
+      circleId={circleId || ""}
+      userId={session?.user?.id || ""}
+      circle={circle}
+    />
+  ) : (
+    <Center w="full" h="full">
+      権限がありません
+    </Center>
   )
 }
 
---
+++ components/forms/activity-form.tsx
@@ -0,0 +1,224 @@
+"use client"
+import { DatePicker } from "@yamada-ui/calendar"
+import type {
+  FC} from "@yamada-ui/react";
+import {
+  Button,
+  Center,
+  FormControl,
+  Heading,
+  Input,
+  Label,
+  Textarea,
+  VStack,
+} from "@yamada-ui/react"
+import Link from "next/link"
+import type { getCircleById } from "@/data/circle"
+
+interface ActivityFormProps {
+  userId: string
+  circleId: string
+  circle?: Awaited<ReturnType<typeof getCircleById>>
+}
+
+export const ActivityForm: FC<ActivityFormProps> = ({ circleId, circle }) => {
+  return (
+    <VStack
+      w="full"
+      h="full"
+      as="form"
+      onSubmit={(e) => e.preventDefault()}
+      p="md"
+    >
+      <VStack p="md" maxW="5xl" m="auto" gap="lg" flexGrow={1}>
+        <Heading>{`${circle?.name} 活動日程`}</Heading>
+        <FormControl
+          display="flex"
+          flexDirection={{ base: "row", md: "column" }}
+          gap={{ base: "2xl", md: "md" }}
+          maxW="2xl"
+          // isInvalid={!!errors.name}
+        >
+          <Label flexGrow={1} isRequired>
+            見出し
+          </Label>
+          <VStack w="auto">
+            <Input
+              type="text"
+              w={{ base: "md", md: "full" }}
+              placeholder="見出しを入力"
+              // {...register("name")}
+            />
+            {/* {errors.name ? (
+                      <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
+                  ) : (
+                      <>
+                          <Spacer />
+                          <Spacer />
+                      </>
+                  )} */}
+          </VStack>
+        </FormControl>
+        <FormControl
+          display="flex"
+          flexDirection={{ base: "row", md: "column" }}
+          gap={{ base: "2xl", md: "md" }}
+          maxW="2xl"
+          // isInvalid={!!errors.name}
+        >
+          <Label flexGrow={1}>内容</Label>
+          <VStack w="auto">
+            <Textarea
+              w={{ base: "md", md: "full" }}
+              placeholder="活動内容を入力"
+              autosize
+              // {...register("name")}
+            />
+            {/* {errors.name ? (
+                      <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
+                  ) : (
+                      <>
+                          <Spacer />
+                          <Spacer />
+                      </>
+                  )} */}
+          </VStack>
+        </FormControl>
+        <FormControl
+          display="flex"
+          flexDirection={{ base: "row", md: "column" }}
+          gap={{ base: "2xl", md: "md" }}
+          maxW="2xl"
+          // isInvalid={!!errors.name}
+        >
+          <Label flexGrow={1} isRequired>
+            日付
+          </Label>
+          <VStack w="auto">
+            <DatePicker
+              locale="ja"
+              w={{ base: "md", md: "full" }}
+              placeholder="YYYY/MM/DD"
+            />
+            {/* {errors.name ? (
+                      <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
+                  ) : (
+                      <>
+                          <Spacer />
+                          <Spacer />
+                      </>
+                  )} */}
+          </VStack>
+        </FormControl>
+        <FormControl
+          display="flex"
+          flexDirection={{ base: "row", md: "column" }}
+          gap={{ base: "2xl", md: "md" }}
+          maxW="2xl"
+          // isInvalid={!!errors.name}
+        >
+          <Label flexGrow={1}>場所</Label>
+          <VStack w="auto">
+            <Input
+              type="text"
+              w={{ base: "md", md: "full" }}
+              placeholder="場所を入力"
+              // {...register("name")}
+            />
+            {/* {errors.name ? (
+                      <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
+                  ) : (
+                      <>
+                          <Spacer />
+                          <Spacer />
+                      </>
+                  )} */}
+          </VStack>
+        </FormControl>
+        <FormControl
+          display="flex"
+          flexDirection={{ base: "row", md: "column" }}
+          gap={{ base: "2xl", md: "md" }}
+          maxW="2xl"
+          // isInvalid={!!errors.name}
+        >
+          <Label flexGrow={1} isRequired>
+            開始時間
+          </Label>
+          <VStack w="auto">
+            <Input
+              type="time"
+              w={{ base: "md", md: "full" }}
+              placeholder="開始時間を入力"
+              // {...register("name")}
+            />
+            {/* {errors.name ? (
+                      <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
+                  ) : (
+                      <>
+                          <Spacer />
+                          <Spacer />
+                      </>
+                  )} */}
+          </VStack>
+        </FormControl>
+        <FormControl
+          display="flex"
+          flexDirection={{ base: "row", md: "column" }}
+          gap={{ base: "2xl", md: "md" }}
+          maxW="2xl"
+          // isInvalid={!!errors.name}
+        >
+          <Label flexGrow={1}>終了時間</Label>
+          <VStack w="auto">
+            <Input
+              type="time"
+              w={{ base: "md", md: "full" }}
+              placeholder="終了時間を入力"
+              // {...register("name")}
+            />
+            {/* {errors.name ? (
+                      <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
+                  ) : (
+                      <>
+                          <Spacer />
+                          <Spacer />
+                      </>
+                  )} */}
+          </VStack>
+        </FormControl>
+        <FormControl
+          display="flex"
+          flexDirection={{ base: "row", md: "column" }}
+          gap={{ base: "2xl", md: "md" }}
+          maxW="2xl"
+          // isInvalid={!!errors.name}
+        >
+          <Label flexGrow={1}>備考</Label>
+          <VStack w="auto">
+            <Textarea
+              w={{ base: "md", md: "full" }}
+              placeholder="備考欄"
+              autosize
+              // {...register("name")}
+            />
+            {/* {errors.name ? (
+                      <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
+                  ) : (
+                      <>
+                          <Spacer />
+                          <Spacer />
+                      </>
+                  )} */}
+          </VStack>
+        </FormControl>
+        <Center gap="md" justifyContent="end">
+          <Button as={Link} href={`/circles/${circleId || ""}/activities`}>
+            キャンセル
+          </Button>
+          <Button type="submit">追加</Button>
+        </Center>
+      </VStack>
+    </VStack>
+  )
+}
