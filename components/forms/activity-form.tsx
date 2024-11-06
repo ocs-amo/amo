c214f96bc1529af093dd8575846138028ec76a12
yuto ichinose <yuto.0526.sano@gmail.com>
2024-11-06 13:50:24 +0900

    feat: フロントのzod実装

--- components/forms/activity-form.tsx
+++ components/forms/activity-form.tsx
@@ -1,19 +1,24 @@
 "use client"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { DatePicker } from "@yamada-ui/calendar"
-import type {
-  FC} from "@yamada-ui/react";
+import type { FC } from "@yamada-ui/react"
 import {
   Button,
   Center,
+  ErrorMessage,
   FormControl,
   Heading,
   Input,
   Label,
+  Spacer,
   Textarea,
   VStack,
 } from "@yamada-ui/react"
 import Link from "next/link"
+import { Controller, useForm } from "react-hook-form"
 import type { getCircleById } from "@/data/circle"
+import type { ActivityFormType} from "@/schema/activity";
+import { ActivitySchema } from "@/schema/activity"
 
 interface ActivityFormProps {
   userId: string
@@ -22,12 +27,32 @@ interface ActivityFormProps {
 }
 
 export const ActivityForm: FC<ActivityFormProps> = ({ circleId, circle }) => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<ActivityFormType>({
+    resolver: zodResolver(ActivitySchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      location: "",
+      date: undefined,
+      startTime: "",
+      endTime: "",
+      notes: "",
+    },
+  })
+  const onSubmit = async (values: ActivityFormType) => {
+    console.log(values)
+  }
   return (
     <VStack
       w="full"
       h="full"
       as="form"
-      onSubmit={(e) => e.preventDefault()}
+      onSubmit={handleSubmit(onSubmit)}
       p="md"
     >
       <VStack p="md" maxW="5xl" m="auto" gap="lg" flexGrow={1}>
@@ -37,7 +62,7 @@ export const ActivityForm: FC<ActivityFormProps> = ({ circleId, circle }) => {
           flexDirection={{ base: "row", md: "column" }}
           gap={{ base: "2xl", md: "md" }}
           maxW="2xl"
-          // isInvalid={!!errors.name}
+          isInvalid={!!errors.title}
         >
           <Label flexGrow={1} isRequired>
             見出し
@@ -47,16 +72,16 @@ export const ActivityForm: FC<ActivityFormProps> = ({ circleId, circle }) => {
               type="text"
               w={{ base: "md", md: "full" }}
               placeholder="見出しを入力"
-              // {...register("name")}
+              {...register("title")}
             />
-            {/* {errors.name ? (
-                      <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
-                  ) : (
-                      <>
-                          <Spacer />
-                          <Spacer />
-                      </>
-                  )} */}
+            {errors.title ? (
+              <ErrorMessage mt={0}>{errors.title.message}</ErrorMessage>
+            ) : (
+              <>
+                <Spacer />
+                <Spacer />
+              </>
+            )}
           </VStack>
         </FormControl>
         <FormControl
@@ -64,7 +89,7 @@ export const ActivityForm: FC<ActivityFormProps> = ({ circleId, circle }) => {
           flexDirection={{ base: "row", md: "column" }}
           gap={{ base: "2xl", md: "md" }}
           maxW="2xl"
-          // isInvalid={!!errors.name}
+          isInvalid={!!errors.description}
         >
           <Label flexGrow={1}>内容</Label>
           <VStack w="auto">
@@ -72,16 +97,16 @@ export const ActivityForm: FC<ActivityFormProps> = ({ circleId, circle }) => {
               w={{ base: "md", md: "full" }}
               placeholder="活動内容を入力"
               autosize
-              // {...register("name")}
+              {...register("description")}
             />
-            {/* {errors.name ? (
-                      <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
-                  ) : (
-                      <>
-                          <Spacer />
-                          <Spacer />
-                      </>
-                  )} */}
+            {errors.description ? (
+              <ErrorMessage mt={0}>{errors.description.message}</ErrorMessage>
+            ) : (
+              <>
+                <Spacer />
+                <Spacer />
+              </>
+            )}
           </VStack>
         </FormControl>
         <FormControl
@@ -89,33 +114,42 @@ export const ActivityForm: FC<ActivityFormProps> = ({ circleId, circle }) => {
           flexDirection={{ base: "row", md: "column" }}
           gap={{ base: "2xl", md: "md" }}
           maxW="2xl"
-          // isInvalid={!!errors.name}
+          isInvalid={!!errors.date}
         >
-          <Label flexGrow={1} isRequired>
-            日付
-          </Label>
-          <VStack w="auto">
-            <DatePicker
-              locale="ja"
-              w={{ base: "md", md: "full" }}
-              placeholder="YYYY/MM/DD"
-            />
-            {/* {errors.name ? (
-                      <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
+          <Controller
+            name="date"
+            control={control}
+            render={({ field }) => (
+              <>
+                <Label flexGrow={1} isRequired>
+                  日付
+                </Label>
+                <VStack w="auto">
+                  <DatePicker
+                    locale="ja"
+                    w={{ base: "md", md: "full" }}
+                    placeholder="YYYY/MM/DD"
+                    {...field}
+                  />
+                  {errors.date ? (
+                    <ErrorMessage mt={0}>{errors.date.message}</ErrorMessage>
                   ) : (
-                      <>
-                          <Spacer />
-                          <Spacer />
-                      </>
-                  )} */}
-          </VStack>
+                    <>
+                      <Spacer />
+                      <Spacer />
+                    </>
+                  )}
+                </VStack>
+              </>
+            )}
+          />
         </FormControl>
         <FormControl
           display="flex"
           flexDirection={{ base: "row", md: "column" }}
           gap={{ base: "2xl", md: "md" }}
           maxW="2xl"
-          // isInvalid={!!errors.name}
+          isInvalid={!!errors.location}
         >
           <Label flexGrow={1}>場所</Label>
           <VStack w="auto">
@@ -123,16 +157,16 @@ export const ActivityForm: FC<ActivityFormProps> = ({ circleId, circle }) => {
               type="text"
               w={{ base: "md", md: "full" }}
               placeholder="場所を入力"
-              // {...register("name")}
+              {...register("location")}
             />
-            {/* {errors.name ? (
-                      <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
-                  ) : (
-                      <>
-                          <Spacer />
-                          <Spacer />
-                      </>
-                  )} */}
+            {errors.location ? (
+              <ErrorMessage mt={0}>{errors.location.message}</ErrorMessage>
+            ) : (
+              <>
+                <Spacer />
+                <Spacer />
+              </>
+            )}
           </VStack>
         </FormControl>
         <FormControl
@@ -140,7 +174,7 @@ export const ActivityForm: FC<ActivityFormProps> = ({ circleId, circle }) => {
           flexDirection={{ base: "row", md: "column" }}
           gap={{ base: "2xl", md: "md" }}
           maxW="2xl"
-          // isInvalid={!!errors.name}
+          isInvalid={!!errors.startTime}
         >
           <Label flexGrow={1} isRequired>
             開始時間
@@ -150,16 +184,16 @@ export const ActivityForm: FC<ActivityFormProps> = ({ circleId, circle }) => {
               type="time"
               w={{ base: "md", md: "full" }}
               placeholder="開始時間を入力"
-              // {...register("name")}
+              {...register("startTime")}
             />
-            {/* {errors.name ? (
-                      <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
-                  ) : (
-                      <>
-                          <Spacer />
-                          <Spacer />
-                      </>
-                  )} */}
+            {errors.startTime ? (
+              <ErrorMessage mt={0}>{errors.startTime.message}</ErrorMessage>
+            ) : (
+              <>
+                <Spacer />
+                <Spacer />
+              </>
+            )}
           </VStack>
         </FormControl>
         <FormControl
@@ -167,7 +201,7 @@ export const ActivityForm: FC<ActivityFormProps> = ({ circleId, circle }) => {
           flexDirection={{ base: "row", md: "column" }}
           gap={{ base: "2xl", md: "md" }}
           maxW="2xl"
-          // isInvalid={!!errors.name}
+          isInvalid={!!errors.endTime}
         >
           <Label flexGrow={1}>終了時間</Label>
           <VStack w="auto">
@@ -175,16 +209,16 @@ export const ActivityForm: FC<ActivityFormProps> = ({ circleId, circle }) => {
               type="time"
               w={{ base: "md", md: "full" }}
               placeholder="終了時間を入力"
-              // {...register("name")}
+              {...register("endTime")}
             />
-            {/* {errors.name ? (
-                      <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
-                  ) : (
-                      <>
-                          <Spacer />
-                          <Spacer />
-                      </>
-                  )} */}
+            {errors.endTime ? (
+              <ErrorMessage mt={0}>{errors.endTime.message}</ErrorMessage>
+            ) : (
+              <>
+                <Spacer />
+                <Spacer />
+              </>
+            )}
           </VStack>
         </FormControl>
         <FormControl
@@ -192,7 +226,7 @@ export const ActivityForm: FC<ActivityFormProps> = ({ circleId, circle }) => {
           flexDirection={{ base: "row", md: "column" }}
           gap={{ base: "2xl", md: "md" }}
           maxW="2xl"
-          // isInvalid={!!errors.name}
+          isInvalid={!!errors.notes}
         >
           <Label flexGrow={1}>備考</Label>
           <VStack w="auto">
@@ -200,16 +234,16 @@ export const ActivityForm: FC<ActivityFormProps> = ({ circleId, circle }) => {
               w={{ base: "md", md: "full" }}
               placeholder="備考欄"
               autosize
-              // {...register("name")}
+              {...register("notes")}
             />
-            {/* {errors.name ? (
-                      <ErrorMessage mt={0}>{errors.name.message}</ErrorMessage>
-                  ) : (
-                      <>
-                          <Spacer />
-                          <Spacer />
-                      </>
-                  )} */}
+            {errors.notes ? (
+              <ErrorMessage mt={0}>{errors.notes.message}</ErrorMessage>
+            ) : (
+              <>
+                <Spacer />
+                <Spacer />
+              </>
+            )}
           </VStack>
         </FormControl>
         <Center gap="md" justifyContent="end">
---
+++ schema/activity.ts
@@ -0,0 +1,23 @@
+import { z } from "zod"
+
+export const ActivitySchema = z.object({
+  // 必須項目: 見出し（タイトル）
+  title: z.string().trim().min(1, { message: "活動タイトルは必須です。" }),
+  // 任意項目: 内容
+  description: z.string().optional(),
+  // 必須項目: 日付
+  date: z.date({ required_error: "日付を設定してください。" }),
+  // 任意項目: 場所
+  location: z.string().optional(),
+  // 必須項目: 開始時間 (時刻フォーマット)
+  startTime: z.string().min(1, { message: "開始時間を設定してください。" }),
+  // 任意項目: 終了時間 (時刻フォーマット)
+  endTime: z
+    .string()
+
+    .optional(),
+  // 任意項目: 備考
+  notes: z.string().optional(),
+})
+
+export type ActivityFormType = z.infer<typeof ActivitySchema>
