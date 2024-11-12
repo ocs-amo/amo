ea9d5fdb2e01ba7f567e08742017f75c8477a5d2
yuto ichinose <yuto.0526.sano@gmail.com>
2024-11-12 14:38:49 +0900

    fix: ログ削除

--- app/circles/[circle_id]/page.tsx
+++ app/circles/[circle_id]/page.tsx
@@ -28,7 +28,6 @@ export const dynamicParams = false
 
 export const generateStaticParams = async () => {
   const circles = await getCircles()
-  console.log(circles)
 
   if (!circles) {
     return []
