334ab001d08f4efa66a4b6666aa0113e90d5b61f
ymmyo5 <epc1126@yahoo.co.jp>
2024-11-19 09:30:52 +0900

    fix: 活動日程の内容の文字数上限を200文字に変更

--- schema/activity.ts
+++ schema/activity.ts
@@ -10,7 +10,7 @@ export const ActivitySchema = z.object({
   // 任意項目: 内容
   description: z
     .string()
-    .max(100, { message: "100文字以下にしてください。" })
+    .max(200, { message: "200文字以下にしてください。" })
     .optional(),
   // 必須項目: 日付
   date: z.date({ required_error: "日付を設定してください。" }),
