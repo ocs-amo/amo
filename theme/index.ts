001cf6cf547281b0c8f1d3f1baec88fcbaa133b5
yuto ichinose <yuto.0526.sano@gmail.com>
2024-11-20 14:47:11 +0900

    feat: 大理石画像追加・トップページのリデザイン

---
+++ theme/index.ts
@@ -0,0 +1,15 @@
+"use client"
+import type { UsageTheme } from "@yamada-ui/react"
+import { extendTheme } from "@yamada-ui/react"
+
+import styles from "./styles"
+// import components from './components'
+// import tokens from "./tokens/index";
+
+const customTheme: UsageTheme = {
+  styles,
+  // components,
+  //   ...tokens,
+}
+
+export default extendTheme(customTheme)()
---
+++ theme/styles/index.ts
@@ -0,0 +1,4 @@
+import { globalStyle } from "./global-style"
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { globalStyle }
