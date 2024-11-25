60ef37f9fc4fc0e7d9b6349e61d9703c788ce5dd
yuto ichinose <yuto.0526.sano@gmail.com>
2024-11-25 13:04:04 +0900

    fix: 背景画像に`background-size: cover;`指定

--- theme/styles/global-style.ts
+++ theme/styles/global-style.ts
@@ -3,5 +3,6 @@ import type { UIStyle } from "@yamada-ui/react"
 export const globalStyle: UIStyle = {
   body: {
     backgroundImage: "url(/white_marble.png)",
+    backgroundSize: "cover",
   },
 }
