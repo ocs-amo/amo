59dee5f3f3728417d745b88ecbc6ee7454f2adba
yuto ichinose <yuto.0526.sano@gmail.com>
2024-11-25 13:52:09 +0900

    fix: ログイン画面で背景画像が表示されない問題修正

--- theme/styles/global-style.ts
+++ theme/styles/global-style.ts
@@ -2,7 +2,7 @@ import type { UIStyle } from "@yamada-ui/react"
 
 export const globalStyle: UIStyle = {
   body: {
-    backgroundImage: "url(/white_marble.png)",
+    backgroundImage: "url(/images/white_marble.png)",
     backgroundSize: "cover",
   },
 }
