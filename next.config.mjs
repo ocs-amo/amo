4a4e1e921a42dc954094aea724de0319f02fcbf5
yuto ichinose <yuto.0526.sano@gmail.com>
2024-12-01 21:56:41 +0900

    fix: webpack書き直し

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,18 @@
+import path from "path"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: "5mb",
     },
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias, // 既存のエイリアスを保持
+      "@/": path.resolve(process.cwd()), // プロジェクトルートへのエイリアス
+    }
+    return config
   },
 }
 
