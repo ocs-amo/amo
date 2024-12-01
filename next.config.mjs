6de180db75b411335145da8bc0694cb98fdc7ca2
yuto ichinose <yuto.0526.sano@gmail.com>
2024-12-01 21:58:44 +0900

    Revert "fix: webpack書き直し"
    This reverts commit 4a4e1e921a42dc954094aea724de0319f02fcbf5.

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,9 @@
-import path from "path"
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: "5mb",
     },
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias, // 既存のエイリアスを保持
-      "@/": path.resolve(process.cwd()), // プロジェクトルートへのエイリアス
-    }
-    return config
   },
 }
 
