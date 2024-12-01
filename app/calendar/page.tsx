37ce8fb64f58cb5f425d76321516e843994d8544
yuto ichinose <yuto.0526.sano@gmail.com>
2024-12-01 21:51:44 +0900

    Revert "fix: 削除"
    This reverts commit 9f83bbabdaca0319281b15f3b62c49d666bb963a.

---
+++ app/calendar/page.tsx
@@ -0,0 +1,15 @@
+import { auth } from "@/auth"
+import { CalendarPage } from "@/components/layouts/calendar"
+import { getMonthlyEvents } from "@/data/activity"
+
+export const metadata = {
+  title: "カレンダー - CIRCLIA",
+}
+
+const Calendar = async () => {
+  const session = await auth()
+  const events = await getMonthlyEvents(session?.user?.id || "", new Date())
+  return <CalendarPage events={events} userId={session?.user?.id || ""} />
+}
+
+export default Calendar
