83831c25b94f431341f75f7a678468e3fb0db90b
yuto ichinose <yuto.0526.sano@gmail.com>
2024-12-01 20:38:01 +0900

    build: webpack設定追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
+import path from "path"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: "5mb",
     },
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname) // プロジェクトルートにマッピング
+    return config
   },
 }
 
