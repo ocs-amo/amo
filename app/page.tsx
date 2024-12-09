16b034539defcd8b0bd47d3dbd99c7aea44ab5fb
yuto ichinose <yuto.0526.sano@gmail.com>
2024-12-09 14:52:51 +0900

    fix: ホームでも表示するように修正

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,10 @@ import {
   VStack,
 } from "@yamada-ui/react"
 import Link from "next/link"
-import { getCirclesByUserId } from "@/actions/circle/fetch-circle"
+import {
+  getCirclesByInstructorId,
+  getCirclesByUserId,
+} from "@/actions/circle/fetch-circle"
 import { getUserById } from "@/actions/user/user"
 import { auth } from "@/auth"
 import { CircleCard } from "@/components/data-display/circle-card"
@@ -30,6 +33,9 @@ export default async function Home() {
   const session = await auth()
   const user = await getUserById(session?.user?.id || "")
   const circles = await getCirclesByUserId(user?.id || "")
+  const instructorCircles = user?.instructorFlag
+    ? await getCirclesByInstructorId(user.id || "")
+    : []
   const announcements = await getAnnouncementsByUserId(user?.id || "")
   const calendarData = await getWeeklyActivities(user?.id || "")
 
@@ -57,7 +63,14 @@ export default async function Home() {
         w="full"
         flexGrow={1}
         templateAreas={{
-          base: `
+          base: user?.instructorFlag
+            ? `
+        "avatar instructor-circles instructor-circles instructor-circles" 
+        "avatar circles circles circles" 
+        "notification calendar calendar calendar" 
+        "notification calendar calendar calendar" 
+        `
+            : `
         "avatar circles circles circles" 
         "avatar circles circles circles" 
         "notification calendar calendar calendar" 
@@ -105,6 +118,43 @@ export default async function Home() {
             <NotificationList announcements={announcements} />
           </CardBody>
         </GridItem>
+        {user?.instructorFlag && (
+          <GridItem as={Card} variant="unstyled" area="instructor-circles">
+            <CardHeader>
+              <Heading as="h3" size="sm">
+                講師担当サークル
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Grid
+                gridTemplateColumns={
+                  circles?.length
+                    ? {
+                        base: "repeat(3, 1fr)",
+                        xl: "repeat(2, 1fr)",
+                        lg: "repeat(1, 1fr)",
+                        md: "repeat(2, 1fr)",
+                        sm: "repeat(1, 1fr)",
+                      }
+                    : undefined
+                }
+                gap="md"
+                w="full"
+                h="full"
+              >
+                {instructorCircles?.length ? (
+                  instructorCircles?.map((data) => (
+                    <CircleCard key={data.id} data={data} />
+                  ))
+                ) : (
+                  <Center w="full" h="full" as={VStack}>
+                    <Text>講師を担当していません</Text>
+                  </Center>
+                )}
+              </Grid>
+            </CardBody>
+          </GridItem>
+        )}
         <GridItem as={Card} variant="unstyled" area="circles">
           <CardHeader>
             <Heading as="h3" size="sm">
